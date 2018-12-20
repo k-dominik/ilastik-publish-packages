51cb11f864a69b290b1b91396f04b329d9327521
k-dominik <k-dominik@users.noreply.github.com>
2018-12-20 14:25:08 +0100

    bumpversion 0.2.1dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="publish-conda-stack",
-    version="0.2.0dev7",
+    version="0.2.1dev0",
     author="Stuart Berg, Carsten Haubold",
     author_email="team@ilastik.org",
     license="MIT",
